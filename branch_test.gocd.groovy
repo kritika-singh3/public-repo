8eb8ac2e80cf7db3b3dd3823393d9fc639c968b6
krazybird <49669198+krazybird@users.noreply.github.com>
2020-09-04 11:37:21 +0530

    Create branch_test.gocd.groovy

---
+++ branch_test.gocd.groovy
@@ -0,0 +1,46 @@
+/*
+ * This file was automatically exported by the GoCD Groovy DSL Plugin.
+ */
+
+GoCD.script {
+  branches {
+    matching {
+//      working for normal git
+//      pattern= ~/refs\/heads\/.*/
+//      from = git {
+//        url = "http://github.com/krazybird/public-repo"
+//        materialUsername = "krazybird"
+//        materialPassword = lookup('github.auth.token')
+//      }
+      from = github {
+        fullRepoName = 'krazybird/public-repo'
+        materialUrl = "https://github.com/krazybird/public-repo"
+        apiAuthToken = ""
+        materialUsername = "krazybird"
+        materialPassword = ""
+//        materialPassword = lookup('github.auth.token')
+      }
+
+      onMatch { ctx ->
+        pipeline("testing-branch-${ctx.branchSanitized}") {
+          group = "testing"
+
+          materials { add(ctx.repo) }
+          stages {
+            stage('stage') {
+              jobs {
+                job('job') {
+                  tasks {
+                    exec { commandLine = ['sleep', '30'] }
+                    exec { commandLine = ['ls'] }
+                  }
+                }
+              }
+            }
+          }
+        }
+      }
+    }
+  }
+}
+
